69c2edd6f141658afb4e5f7ad7d2ffd8f8ad5234
jeff <mswintel@live.com>
2020-05-01 16:43:11 +0800

    Update

--- DataStructure/DataStructure.cpp
+++ DataStructure/DataStructure.cpp
@@ -31,6 +31,8 @@ public:
 void test_vector() {
   adt::Vector<int> vt;
 
+  cout << endl << "test vector:" << endl;
+
   cout << "push_back 10|12" << endl;
   vt.push_back(10);
   vt.push_back(12);
@@ -80,8 +82,76 @@ void test_vector() {
   v3.dump();
 }
 
+void test_list() {
+  adt::List<int> vt;
+
+  cout << endl << "test list:" << endl;
+
+  cout << "push_back 10|12|19|20" << endl;
+  vt.push_back(10);
+  vt.push_back(12);
+  vt.dump();
+
+  cout << "emplace 19|20 after 12" << endl;
+  auto it = vt.begin();
+  ++it;
+  it = vt.emplace(it, 19);
+  it = vt.emplace(it, 20);
+  vt.dump();
+
+  cout << "emplace_back 5|8" << endl;
+  vt.emplace_back(5);
+  vt.emplace_back(8);
+  vt.dump();
+
+  cout << "size" << endl;
+  cout << vt.size() << endl;
+
+  cout << "front - back" << endl;
+  cout << vt.front() << " - " << vt.back() << endl;
+
+  cout << "resize to 20" << endl;
+  vt.resize(20);
+  vt.dump();
+
+  cout << "resize to 4" << endl;
+  vt.resize(4);
+  vt.dump();
+
+  cout << "change front to 10000" << endl;
+  vt.front() = 10000;
+  vt.dump();
+
+  cout << "insert 5 after 10000" << endl;
+  it = vt.insert(vt.begin(), 5);
+  vt.dump();
+
+  cout << "insert 6 dup 3 after 5" << endl;
+  vt.insert(it, (size_t)3, 6);
+  vt.dump();
+
+  cout << "copy cotor" << endl;
+  adt::List<int> v2 = adt::List<int>(vt);
+  v2.dump();
+
+  cout << "move cotor" << endl;
+  adt::List<int> v3 = adt::List<int>(std::move(v2));
+  v3.dump();
+
+  cout << "remove 6" << endl;
+  v3.remove(6);
+  v3.dump();
+
+  cout << "clear" << endl;
+  v3.clear();
+  v3.dump();
+}
+
 int main() {
+
+  std::list<int> k;
   test_vector();
+  test_list();
 
   std::cout << "Hello World!\n";
 }
--- DataStructure/List.h
+++ DataStructure/List.h
@@ -2,6 +2,7 @@
 
 #include "Allocator.h"
 #include <assert.h>
+#include <iostream>
 #include <memory>
 #include <vector>
 
@@ -19,7 +20,62 @@ template <typename Ty> struct ListNode {
   template <typename... ValTy>
   explicit ListNode(ValTy &&... V) : Value(std::forward<ValTy>(V)...) {}
 
-  ~ListNode() = default;
+  ~ListNode() { Value.~Ty(); }
+};
+
+template <typename Ty> class ListConstIterator {
+public:
+  using node_ptr = ListNode<Ty> *;
+  using iterator_category = std::bidirectional_iterator_tag;
+  using value_type = Ty;
+  using pointer = const Ty *;
+  using reference = const Ty &;
+  using difference_type = int;
+
+public:
+  ListConstIterator() : node_(nullptr) {}
+
+  ListConstIterator(node_ptr Ptr) : node_(Ptr) {}
+
+  ListConstIterator &operator++() {
+    node_ = node_->Next;
+    return *this;
+  }
+
+  ListConstIterator &operator--() {
+    node_ = node_->Prev;
+    return *this;
+  }
+
+  const ListConstIterator operator++(int) {
+    ListConstIterator it = *this;
+    ++*this;
+    return it;
+  }
+
+  const ListConstIterator operator--(int) {
+    ListConstIterator it = *this;
+    --*this;
+    return it;
+  }
+
+  reference operator*() const { return (reference)node_->Value; }
+
+  pointer operator->() const { return (pointer)&node_->Value; }
+
+  bool operator!=(const ListConstIterator &another) { return !IsSame(another); }
+
+  bool operator==(const ListConstIterator &another) { return IsSame(another); }
+
+  node_ptr data() { return node_; }
+
+private:
+  bool IsSame(const ListConstIterator &another) {
+    return node_ == another.node_;
+  }
+
+private:
+  node_ptr node_;
 };
 
 template <typename Ty> class ListIterator {
@@ -57,17 +113,17 @@ public:
     return it;
   }
 
-  reference operator->() { return node_->Value; }
-
-  const_reference operator->() const { return (const_reference)node_->Value; }
-
   reference operator*() { return node_->Value; }
 
-  const_reference operator*() const { return node_->Value; }
+  const_reference operator*() const { return (const_reference)node_->Value; }
+
+  pointer operator->() { return &node_->Value; }
 
   bool operator!=(const ListIterator &another) { return !IsSame(another); }
 
   bool operator==(const ListIterator &another) { return IsSame(another); }
+
+  node_ptr data() { return node_; }
 
 private:
   bool IsSame(const ListIterator &another) { return node_ == another.node_; }
@@ -84,7 +140,7 @@ public:
   using const_reference = const Ty &;
   using value_type = Ty;
   using iterator = ListIterator<Ty>;
-  using const_iterator = const ListIterator<Ty>;
+  using const_iterator = ListConstIterator<Ty>;
 
   using al_ = AllocatorTy;
 
@@ -93,6 +149,26 @@ public:
 
   explicit List(const_reference Element) : head_(Ty()) {
     head_.Next = head_.Prev = &head_;
+  }
+
+  explicit List(const List &another) {
+    head_.Next = head_.Prev = &head_;
+    insert(end(), another.begin(), another.end());
+  }
+
+  explicit List(List &&another) {
+    if (another.size() == 0) {
+      head_.Next = head_.Prev = &head_;
+      return;
+    }
+    node_ptr headA = another.head_.Next;
+    node_ptr tailA = another.head_.Prev;
+    headA->Prev = &head_;
+    tailA->Next = &head_;
+    head_.Next = headA;
+    head_.Prev = tailA;
+
+    size_ = another.size_;
   }
 
   void push_back(const_reference Element) {
@@ -123,6 +199,13 @@ public:
   template <typename... ValTy> void emplace_front(ValTy &&... Element) {
     InsertHead(std::forward<ValTy>(Element)...);
     ++size_;
+  }
+
+  template <typename... ValTy>
+  iterator emplace(iterator Where, ValTy &&... Element) {
+    iterator pos = InsertAfter(Where.data(), std::forward<ValTy>(Element)...);
+    ++size_;
+    return pos;
   }
 
   void pop_front() {
@@ -161,22 +244,67 @@ public:
 
   const_iterator begin() const {
     assert(head_.Next != &head_);
-    return (const_iterator)iterator(head_.Next);
+    return const_iterator(head_.Next);
   }
 
-  iterator end() {
-    assert(head_.Next != &head_);
-    return iterator(&head_);
+  iterator end() { return iterator(GetHead()); }
+
+  const_iterator end() const { return const_iterator((const node_ptr)&head_); }
+
+  const_iterator cbegin() const { return begin(); }
+  const_iterator cend() const { return end(); }
+
+  template <typename Iterator>
+  void insert(iterator Where, Iterator First, Iterator Last) {
+    iterator cur = Where;
+    for (Iterator iter = First; iter != Last; ++iter) {
+      cur = emplace(cur, *iter);
+    }
   }
 
-  const_iterator end() const {
-    assert(head_.Next != &head_);
-    return (const_iterator)iterator(&head_);
+  iterator insert(iterator Where, const Ty &Val) { return emplace(Where, Val); }
+
+  iterator insert(iterator Where, size_t Count, const Ty &Val) {
+    iterator cur = Where;
+    for (size_t i = 0; i < Count; ++i) {
+      cur = emplace(cur, Val);
+    }
+    return cur;
   }
 
-  template <typename Iterator> void insert(Iterator First, Iterator Last) {
-    for (Iterator iter = First; iter != Last; ++iter)
-      push_back(*iter);
+  iterator insert(iterator Where, size_t Count, Ty &&Val) {
+    iterator cur = Where;
+    for (size_t i = 0; i < Count; ++i) {
+      cur = emplace(cur, Val);
+    }
+    return cur;
+  }
+
+  iterator insert(iterator Where, std::initializer_list<Ty> Init) {
+    insert(Where, Init.begin(), Init.end());
+  }
+
+  void remove(const Ty &Val) {
+    if (size() == 0)
+      return;
+    iterator it = begin();
+    while (it != end()) {
+      if (*it == Val) {
+        it = UnlinkAndDelete(it.data());
+      } else {
+        ++it;
+      }
+    }
+  }
+
+  void clear() {
+    if (size() == 0)
+      return;
+    iterator it = begin();
+    while (it != end()) {
+      it = UnlinkAndDelete(it.data());
+    }
+    size_ = 0;
   }
 
   size_t size() const { return size_; }
@@ -212,6 +340,16 @@ public:
       }
   }
 
+  void dump() {
+    std::cout << "[";
+    if (size() != 0) {
+      for (iterator it = begin(); it != end(); ++it) {
+        std::cout << *it << ",";
+      }
+    }
+    std::cout << "]" << std::endl;
+  }
+
 private:
   /// @brief:创建一个新结点，自动完成构造
   template <typename... ValTy> node_ptr NewNode(ValTy &&... Element) {
@@ -221,49 +359,62 @@ private:
     return new_node;
   }
 
+  /// @brief:在Where之后插入一个元素
+  template <typename... ValTy>
+  iterator InsertAfter(node_ptr Where, ValTy &&... Element) {
+    node_ptr new_node = NewNode(std::forward<ValTy>(Element)...);
+    new_node->Next = Where->Next;
+    new_node->Prev = Where;
+    Where->Next->Prev = new_node;
+    Where->Next = new_node;
+    return iterator(new_node);
+  }
+
+  /// @brief:在Where之后插入一个元素
+  template <typename... ValTy>
+  iterator InsertBefore(node_ptr Where, ValTy &&... Element) {
+    node_ptr new_node = NewNode(std::forward<ValTy>(Element)...);
+    new_node->Next = Where;
+    new_node->Prev = Where->Prev;
+    Where->Prev->Next = new_node;
+    Where->Prev = new_node;
+    return iterator(new_node);
+  }
+
   /// @brief:在链表尾部插入一个元素
   template <typename... ValTy> void InsertTail(ValTy &&... Element) {
-    node_ptr new_node = NewNode(std::forward<ValTy>(Element)...);
-    new_node->Next = &head_;
-    new_node->Prev = head_.Prev;
-    head_.Prev->Next = new_node;
-    head_.Prev = new_node;
+    InsertBefore(GetHead(), std::forward<ValTy>(Element)...);
   }
 
   /// @breif:在链表头部插入一个元素
   template <typename... ValTy> void InsertHead(ValTy &&... Element) {
-    node_ptr new_node = NewNode(std::forward<ValTy>(Element)...);
-    new_node->Next = head_.Next;
-    new_node->Prev = &head_;
-    head_.Next->Prev = new_node;
-    head_.Next = new_node;
+    InsertAfter(GetHead(), std::forward<ValTy>(Element)...);
+  }
+
+  /// @brief:将一个元素移出链表并销毁
+  iterator UnlinkAndDelete(node_ptr Where) {
+    node_ptr prev = Where->Prev;
+    node_ptr next = Where->Next;
+    prev->Next = next;
+    next->Prev = prev;
+
+    Where->~ListNode();
+    al_::Deallocate(Where);
+
+    return iterator(next);
   }
 
   /// @brief:删除链表尾部的结点
-  void RemoveTail() {
-    node_ptr victim = head_.Prev;
-    node_ptr new_tail = victim->Prev;
-    new_tail->Next = &head_;
-    head_.Prev = new_tail;
-
-    victim->~ListNode();
-    al_::Deallocate(victim);
-  }
+  void RemoveTail() { UnlinkAndDelete(GetHead()->Prev); }
 
   /// @brief:删除链表头部的结点
-  void RemoveHead() {
-    node_ptr victim = head_.Next;
-    node_ptr new_head = victim->Next;
-    new_head->Prev = &head_;
-    head_.Next = new_head;
+  void RemoveHead() { UnlinkAndDelete(GetHead()->Next); }
 
-    victim->~ListNode();
-    al_::Deallocate(victim);
-  }
+  node_ptr GetHead() { return &head_; };
 
 private:
   node head_;
   size_t size_ = 0;
-};
+}; // namespace adt
 
 } // namespace adt
